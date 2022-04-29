ee1f7abfb95f9022e9723a449d6fe335916c0bc6
linetlucy-genchabe <linetlucy21@gmail.com>
2022-04-29 22:00:24 +0300

    rename run

--- app/__init__.py
+++ app/__init__.py
@@ -17,4 +17,8 @@ def create_app(config_name):
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
 
+    # setting config 
+    from .request import configure_request
+    configure_request(app)
+
     return app
