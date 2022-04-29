450b728fc807556b96ae6247664b95ea9d0c082d
linetlucy-genchabe <linetlucy21@gmail.com>
2022-04-29 20:12:11 +0300

    add classes

---
+++ config.py
@@ -0,0 +1,35 @@
+import os
+
+class Config:
+    '''
+    General configuration parent class
+    '''
+    NEWS_API_BASE_URL ='https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey={}'
+    NEWS_API_KEY = '93509c9facdd417b8083380c748d003e'
+    SECRET_KEY ='lynne'
+
+
+class ProdConfig(Config):
+    '''
+    Production  configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    pass
+
+
+class DevConfig(Config):
+    '''
+    Development  configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+
+    DEBUG = True
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig
+}
