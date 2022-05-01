9533514f96f0ca7eccbb032eaaaf4419976de5b9
linetlucy-genchabe <linetlucy21@gmail.com>
2022-05-01 19:16:15 +0300

    add classes

--- app/models.py
+++ app/models.py
@@ -1,34 +1,45 @@
-
-  
-class Source:
-	'''
-	Source Class to define Source Objects
-	'''
-	def __init__(self,id,name,description,url,category):
-		'''
-		Function to initialize Source Objects
-		It defines the properties each Source object will hold.
-	
-		'''
-		self.id = id
-		self.name = name
-		self.description = description
-		self.url = url
-		self.category = category
-
 class Article:
-	'''
-	Article Class to define Article Objects
-	'''
-	def __init__(self,author,title,description,url,image,date):
-		'''
-		Function to initialize Article Objects
-		It defines the properties each Article object will hold.
-	
-		'''
-		self.author = author
-		self.title = title
-		self.description = description
-		self.url = url
-		self.image = image
-		self.date = date
+    '''
+    Class that instantiates objects of the news article objects of the news sources
+    '''
+    def __init__(self,author,description,time,url,image,title):
+        self.author = author
+        self.description = description
+        self.time = time
+        self.url = url
+        self.image = image
+        self.title = title
+
+class Category:
+    '''
+    Class that instantiates objects of the news categories objects of the news sources
+    '''
+    def __init__(self,author,description,time,url,image,title):
+        self.author = author
+        self.description = description
+        self.time = time
+        self.url = url
+        self.image = image
+        self.title = title
+
+class Source:
+    '''
+    Source class to define source objects
+    '''
+    def __init__(self,id,name,description,url):
+        self.id = id
+        self.name = name
+        self.description = description
+        self.url = url
+
+class Headlines:
+    '''
+    Class that instantiates objects of the headlines categories objects of the news sources
+    '''
+    def __init__(self,author,description,time,url,image,title):
+        self.author = author
+        self.description = description
+        self.time = time
+        self.url = url
+        self.image = image
+        self.title = title
